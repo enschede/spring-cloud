7fbb7af65fda2475bd150f63d35d40e933a670a8
Marc Enschede <marc@marcenschede.nl>
2018-05-13 11:51:49 +0200

    Hystrix

--- component-a/src/main/kotlin/nl/marcenschede/tests/springcloud/componenta/ComponentAApplication.kt
+++ component-a/src/main/kotlin/nl/marcenschede/tests/springcloud/componenta/ComponentAApplication.kt
@@ -1,9 +1,12 @@
 package nl.marcenschede.tests.springcloud.componenta
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand
+import com.netflix.ribbon.proxy.annotation.Hystrix
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.cloud.netflix.hystrix.EnableHystrix
 import org.springframework.cloud.netflix.ribbon.RibbonClient
 import org.springframework.cloud.openfeign.EnableFeignClients
 import org.springframework.cloud.openfeign.FeignClient
@@ -15,19 +18,23 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @EnableFeignClients
 @EnableDiscoveryClient
+@EnableHystrix
 class ComponentAApplication {
 
     @Autowired
     lateinit var proxy: ForecastProxy
 
     @GetMapping("/forecast/{city}")
-    fun createForecast(@PathVariable city: String): ForecastPresenter? {
+    fun createForecast(@PathVariable city: String): ForecastPresenter? =
+        proxy.createForecast(city)
 
-        val forecast = proxy.createForecast(city)
+    @GetMapping("/error/{city}")
+    @HystrixCommand(fallbackMethod = "fallbackForecast")
+    fun errorForecast(@PathVariable city: String): ForecastPresenter? =
+        throw RuntimeException()
 
-        return forecast
-    }
-
+    fun fallbackForecast(@PathVariable city: String): ForecastPresenter? =
+        ForecastPresenter("none", "0", "0")
 }
 
 data class ForecastPresenter(val city: String, val longitude: String, val lattitude: String, var forecast: String = "", var port:Int = 0)
